8622c8d032b8298cbe4bd7599c059898c078bb46
JaySixty <41970914+Jamie9192@users.noreply.github.com>
2025-02-20 22:14:37 +0000

    added shadcn and tailwind

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import {resolve} from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'build',
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
 });
